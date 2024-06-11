4e98189730fdc8522e3b4ae793af579d645a1f7f
Nur Fikri <codingki@gmail.com>
2024-06-11 21:42:50 +0700

    swap widget in ibc fun

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { IbcFunLogo } from "./IbcFunLogo";
 
 function Header() {
   return (
-    <nav className="relative flex items-center justify-center px-4 py-8 sm:py-12">
+    <nav className="flex items-center justify-center px-4 py-8 sm:py-12">
       <AdaptiveLink href="/">
         <IbcFunLogo className="h-8 sm:h-16" />
       </AdaptiveLink>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Footer from "@/components/Footer";
+import { SwapWidget } from "@skip-go/widget";
+
 import Header from "@/components/Header";
 import { Help } from "@/components/Help";
 import SkipBanner from "@/components/SkipBanner";
 import { VersionCheck } from "@/components/VersionCheck";
 import { cn } from "@/utils/ui";
-import { Widget } from "@/widget";
 
 export default function Home() {
   return (
@@ -16,14 +16,19 @@ export default function Home() {
         "before:bg-[url(/site-bg-2.svg)] before:bg-cover before:bg-[center_top] before:bg-no-repeat",
       )}
     >
-      <main className="relative flex min-h-screen flex-col items-center sm:pt-11">
-        <SkipBanner className="inset-x-0 top-0 z-50 w-screen sm:fixed" />
+      <main className="flex min-h-screen flex-col items-center pb-11 sm:pt-11">
+        <SkipBanner className="z-9 inset-x-0 top-0 w-screen sm:fixed" />
         <Header />
         <div className="flex flex-grow flex-col items-center">
-          <div className="relative w-screen overflow-hidden bg-white p-6 shadow-xl sm:max-w-[450px] sm:rounded-3xl">
-            <Widget />
+          <div className="w-screen overflow-hidden shadow-xl sm:max-w-[450px] sm:rounded-3xl">
+            <SwapWidget
+              className="w-full p-6"
+              defaultRoute={{
+                srcChainID: "cosmoshub-4",
+                srcAssetDenom: "uatom",
+              }}
+            />
           </div>
-          <Footer />
         </div>
       </main>
       <VersionCheck />
